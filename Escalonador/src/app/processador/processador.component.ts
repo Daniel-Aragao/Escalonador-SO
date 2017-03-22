e0c7c3b1c5f17446b9a8ab9868724518e44f6757
Thiago Maia da Costa <thiago.maia971@gmail.com>
2017-03-22 20:17:44 -0300

    Incio da implementacao do loop no core.

--- Escalonador/src/app/processador/processador.component.ts
+++ Escalonador/src/app/processador/processador.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ProcessSenderService } from '../services/process-sender.service';
 import { ProcessoViewModel } from '../models/ProcessoViewModel';
 
@@ -10,27 +10,54 @@ import { Subscription } from 'rxjs/Subscription';
   styleUrls: ['./processador.component.css']
 })
 export class ProcessadorComponent implements OnInit {
+
+  @Input()
+  private running: boolean;
   private subscription: Subscription;
-  private cores: any[] = [];
+  private cores: CoreViewModel[] = [];
 
   constructor(private ProcessSenderService: ProcessSenderService) {
+
   }
 
   ngOnInit() {
     this.subscription = this.ProcessSenderService.addProccess.subscribe(
       (p: ProcessoViewModel) => this.addToLine(p));
+
+    this.Loop();
+
   }
 
-    private addToLine(p: ProcessoViewModel) {
+  private addToLine(p: ProcessoViewModel) {
     if (!p.isGroup) {
-      this.cores.push(p.Processo);
+      //this.cores.push(new Pro p);
     } else {
       //p.GrupoProcessos.forEach((v: Processo, i: number, a: Processo[]) => this.addProcessToQueue(v, p.color))
     }
+  }
+
+  private Loop(): void {
+    while(this.running){
+      this.VarrerCores();
+
+    }
+  }
+
+  private VarrerCores(): void {
+    this.cores.forEach(core => {
+      if (!core.Processo) {
+        // pedir processo
+      }
+      
+    });
   }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
 
+}
+
+class CoreViewModel {
+  public Processo: ProcessoViewModel;
 }
