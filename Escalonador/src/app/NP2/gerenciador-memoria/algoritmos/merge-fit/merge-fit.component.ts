59ddc002364cafd1334009d2fe802b8cd4cce243
Daniel Aragão Abreu Filho <danielfilhoce@outlook.com>
2017-06-04 01:17:25 -0300

    Estrutura do best-fit copiada pro merge-fit

--- Escalonador/src/app/NP2/gerenciador-memoria/algoritmos/merge-fit/merge-fit.component.ts
+++ Escalonador/src/app/NP2/gerenciador-memoria/algoritmos/merge-fit/merge-fit.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, EventEmitter, Input, Output } from '@angular/core';
+import { AlocarMemoriaService } from '../../../../services/alocar-memoria.service';
+import { AlocarMemoriaViewModel } from '../../../../models/AlocarMemoriaViewModel';
+import { MemoryMenuViewModel } from '../../../../models/MemoryMenuViewModel';
+import { BlocoMemoria } from '../../../../models/BlocoMemoria';
+import { BlockNode } from '../../../../models/BlockNode';
+import { KillProcessService } from '../../../../services/kill-process.service';
+import { KillProcessViewModel } from "../../../../models/KillProcessViewModel";
+
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'esc-merge-fit',
@@ -6,10 +15,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./merge-fit.component.css']
 })
 export class MergeFitComponent implements OnInit {
+  private AlocarMemoriaSubscription: Subscription;
+  private ProcessKilledSubscriptio: Subscription;
+  
+  @Output("ViewModelEmitter") ViewModelEmitter = new EventEmitter(); // joga a decisão no gerenciador-memoria.component
+  @Input() MemoryVM: MemoryMenuViewModel;
 
-  constructor() { }
+  TodosOsBlocos: BlocoMemoria;
+  UltimoBloco: BlocoMemoria;
 
-  ngOnInit() {
+  BlocosLivres: BlockNode;
+
+  constructor(private AlocarMemoriaService: AlocarMemoriaService, private KillProcessService: KillProcessService) {
+    this.HandleRequisicao = this.HandleRequisicao.bind(this);
   }
 
+  ngOnInit() {
+    this.MemoryVM.MemoriaTotal = this.MemoryVM.size;
+
+    this.AlocarMemoriaSubscription = this.AlocarMemoriaService.handleNewProcess.subscribe(
+      (a: AlocarMemoriaViewModel) => this.HandleRequisicao(a)
+    );
+
+    this.ProcessKilledSubscriptio = this.KillProcessService.handleKilledProcess.subscribe(
+      (kp: KillProcessViewModel) => this.HandleKilledProcess(kp));
+
+  }
+
+  ngOnDestroy() {
+    this.AlocarMemoriaSubscription.unsubscribe();
+    this.ProcessKilledSubscriptio.unsubscribe();
+  }
+
+  HandleKilledProcess(kp: KillProcessViewModel) {
+    this.MoverParaLivre(kp.ProcessoViewModel.Processo.BlocosMemoria);
+  }
+
+  MoverParaLivre(blocos: BlocoMemoria[]): void {    
+    
+  }
+
+  HandleRequisicao(a: AlocarMemoriaViewModel): void {
+
+    // this.ViewModelEmitter.emit(a);
+  }
+
+
+  private hasMemory(requisicao) {
+    return this.MemoryVM.MemoriaTotal > (this.MemoryVM.MemoriaOcupada + requisicao);
+  }
 }
